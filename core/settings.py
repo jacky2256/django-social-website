d3f7ebf348ea28dfc4d068d2e5862e8175d86371
jacky2256 <a.bojic22@gmail.com>
2024-07-03 22:40:50 +0300

    add settings for auth with google

--- core/settings.py
+++ core/settings.py
@@ -160,3 +160,16 @@ SOCIAL_AUTH_JSONFIELD_ENABLED = True
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '81937512947-mpsl6na04jtuh5oo8r2kbkmu7gggfjcl.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]

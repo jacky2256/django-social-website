62ce1e2e4f098403a9a896cc31cc225a03c03355
jacky2256 <a.bojic22@gmail.com>
2024-07-07 14:32:58 +0300

    add pagination images

--- images/urls.py
+++ images/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import image_create, image_detail
+from .views import image_create, image_detail, image_like, image_list
 
 app_name = 'images'
 
 urlpatterns = [
     path('create/', image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/',
-        image_detail, name='detail'),
+         image_detail, name='detail'),
+    path('like/', image_like, name='like'),
+    path('', image_list, name='image_list'),
 ]
